1d37b7c9b49fbe58342082d3b27d76376806df05
Brian Barrow <briancbarrow@gmail.com>
2017-01-24 22:28:42 -0700

    retrieved data into model and displayed into template

--- app/routes/weather.js
+++ app/routes/weather.js
@@ -4,12 +4,10 @@ export default Ember.Route.extend({
   model() {
     return Ember.$.getJSON('http://ipinfo.io').then(data => {
       console.log(data)
-      Ember.$.getJSON('http://api.openweathermap.org/data/2.5/weather?zip=' + data.postal + '&APPID=a62cd1cab307b87f29523ee6112488f5').then(data => {
+      return Ember.$.getJSON('http://api.openweathermap.org/data/2.5/weather?zip=' + data.postal + '&units=imperial&APPID=a62cd1cab307b87f29523ee6112488f5').then(data => {
         console.log(data)
+        return { city: data.name, icon: 'http://openweathermap.org/img/w/' + data.weather[0].icon + '.png', description: data.weather[0].main, temp: data.main.temp}
       })
     })
-    // return Ember.$.getJSON('http://api.openweathermap.org/data/2.5/weather?lat=35&lon=139&APPID=a62cd1cab307b87f29523ee6112488f5').then(data => {
-    //   console.log(data)
-    // })
   }
 });
